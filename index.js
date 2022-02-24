4a7f64d0ece64b5f67869cd87e99df06965055b6
bouncingbumble <jordboudreau@gmail.com>
2022-02-23 17:42:17 -0800

    has won logic

--- index.js
+++ index.js
@@ -77,51 +77,64 @@ app.post('/api/v1/twilio', async (req, res, next) => {
     } else {
         let guess = req.body.Body.toUpperCase().replace(/\s+/g, '')
         console.log(`The user guessed ${guess}`)
-        let winLossMessage = ''
 
-        if (validWords.check(guess.toLowerCase())) {
-            if (guess.length === 5) {
-                try {
-                    let guesses = user.guesses
-
-                    if (!user.hasWon && user.guesses < 6) {
-                        guesses = guesses + 1
-                        user.guesses = guesses
-                        user.wordsTried.push(guess)
-                        await user.save()
-                    }
-
-                    let gameMessage = await isCorrectWord(
-                        guess,
-                        todaysWord,
-                        user
-                    )
-
-                    if (guesses > 5 && guess !== todaysWord) {
-                        guesses = 'X'
-                        user.streak = 0
-                        await user.save()
-                        winLossMessage =
-                            'you lose!\nreply with "word?" to see solution'
-                    }
-
-                    if (guess === todaysWord) {
-                        winLossMessage = 'you win!'
-                    }
-                    response.message(
-                        `boudreaudle ${getBoudreaudleDay()} ${guesses}/6\n${gameMessage} \n ${winLossMessage}`
-                    )
-                } catch (error) {
-                    console.log(error)
-                    next(error)
-                }
-            } else if (guess.length !== 5) {
-                response.message(
-                    `sorry, that is not a 5 letter word, please try again`
-                )
-            }
+        if (user.hasWon) {
+            response.message(
+                `you already won! come back tomorrow (12:00am PST) to play again!`
+            )
         } else {
-            response.message('sorry, that is not a real word, please try again')
+            if (guess === 'WORD?' && user.guesses === 6) {
+                response.message(`bummer, the word was ${todaysWord}`)
+            } else {
+                let winLossMessage = ''
+
+                if (validWords.check(guess.toLowerCase())) {
+                    if (guess.length === 5) {
+                        try {
+                            let guesses = user.guesses
+
+                            if (!user.hasWon && user.guesses < 6) {
+                                guesses = guesses + 1
+                                user.guesses = guesses
+                                user.wordsTried.push(guess)
+                                await user.save()
+                            }
+
+                            let gameMessage = await isCorrectWord(
+                                guess,
+                                todaysWord,
+                                user
+                            )
+
+                            if (guesses > 5 && guess !== todaysWord) {
+                                guesses = 'X'
+                                user.streak = 0
+                                await user.save()
+                                winLossMessage =
+                                    'you lose!\nreply with "word?" to see solution'
+                            }
+
+                            if (guess === todaysWord) {
+                                winLossMessage = 'you win!'
+                            }
+                            response.message(
+                                `boudreaudle ${getBoudreaudleDay()} ${guesses}/6\n${gameMessage} \n ${winLossMessage}`
+                            )
+                        } catch (error) {
+                            console.log(error)
+                            next(error)
+                        }
+                    } else if (guess.length !== 5) {
+                        response.message(
+                            `sorry, that is not a 5 letter word, please try again`
+                        )
+                    }
+                } else {
+                    response.message(
+                        'sorry, that is not a real word, please try again'
+                    )
+                }
+            }
         }
     }
     res.set('Content-Type', 'text/xml')
